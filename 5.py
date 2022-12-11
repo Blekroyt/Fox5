7177c14f1862267554544b2da85c839a81266f7c
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-12-11 14:51:13 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-file = open("newfile1.txt", "x")
-print(file)
-if file:
-    print("File created successfully")
-file.close()
+if __name__ == "__main__":
+    
+    file = open("newfile1.txt", "x")
+    print(file)
+    if file:
+        print("File created successfully")
+    file.close()
