d9c1bc0769ba956bf273fc0cde39012993a3df7c
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-12-11 14:52:12 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-fp = open('file2.txt', 'r')
-print("The filepointer is at byte :",fp.tell())
-content = fp.read();
-print("After reading, the filepointer is at:",fp.tell())
+if __name__ == "__main__":
+  
+    fp = open('file2.txt', 'r')
+    print("The filepointer is at byte :",fp.tell())
+    content = fp.read();
+    print("After reading, the filepointer is at:",fp.tell())
