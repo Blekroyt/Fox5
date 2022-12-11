905d852e7bf6f37d4c79ef452fb4ba504aac4754
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-12-11 14:50:35 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-file = open("file2.txt", "r")
-content = file.readlines()
-print(content)
-file.close()
+if __name__ == "__main__":
+  
+  file = open("file2.txt", "r")
+  content = file.readlines()
+  print(content)
+  file.close()
